889d169807b3fffc900f55bf7c7ac0482279ee95
Jim Gay <jim@saturnflyer.com>
2025-10-18 08:13:14 -0400

    Add coverage reporting
    Added: SimpleCov for coverage reporting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+if ENV["CI"]
+  require "simplecov"
+  SimpleCov.start do
+    add_filter "/test/"
+    enable_coverage :branch
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "klee"
 require "debug"
